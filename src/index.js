1e00a5cdefc5ece46782052bd9bf53aa6e5eadbf
EduardoMarcelinoEng <eduardo.marcelino.eng@gmail.com>
2020-07-23 09:05:09 -0300

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const db = require('./models');
 
 const response = require('./middlewares/response');
@@ -10,6 +12,7 @@ const linkController = require('./controllers/link');
 
 const app = express();
 
+app.use(cors());
 app.use(response);
 app.use(checkJwt);
 
